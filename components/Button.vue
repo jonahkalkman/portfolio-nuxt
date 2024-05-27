1e95b37ac66cf00c5ac7bd3387527022ce276d79
Jonah Kalkman <jonahkalkman@gmail.com>
2024-05-27 23:15:24 +0200

    Fixed combine classes

--- components/Button.vue
+++ components/Button.vue
@@ -1,24 +1,13 @@
 <template>
   <template v-if="link">
-    <NuxtLink
-      :to="link"
-      :class="
-        classes +
-        ' flex items-center justify-center gap-1 py-2 px-5 rounded-full group shadow-lg hover:opacity-80 transition-all duration-300 ease-in-out'
-      "
-    >
+    <NuxtLink :to="link" :class="[linkClasses, classes]">
       <p class="text-xl transition-all duration-300 ease-in-out">
         {{ title }}
       </p>
     </NuxtLink>
   </template>
   <template v-else>
-    <div
-      :class="
-        classes +
-        ' flex items-center justify-center gap-1 py-2 px-5 rounded-full group shadow-lg hover:opacity-80 transition-all duration-300 ease-in-out'
-      "
-    >
+    <div :class="[linkClasses, classes]">
       <p class="text-xl transition-all duration-300 ease-in-out">
         {{ title }}
       </p>
@@ -32,4 +21,7 @@ const { title, link } = defineProps({
   link: { type: String, required: false },
   classes: { type: String, default: "" },
 });
+
+const linkClasses =
+  "flex items-center justify-center gap-1 py-2 px-5 rounded-full group shadow-lg hover:opacity-80 transition-all duration-300 ease-in-out";
 </script>
