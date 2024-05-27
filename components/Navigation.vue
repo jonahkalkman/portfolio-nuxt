bc7ac428a7de06e87bfa6b6beb4dc486f070d38f
Jonah Kalkman <jonahkalkman@gmail.com>
2024-05-27 23:21:52 +0200

    Fixed classes for nav

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,10 +1,14 @@
 <template>
   <nav class="">
     <ul class="flex space-x-4 gap-5">
-      <li :class="route.path == '/work' ? activeLinkClasses : linkClasses ">
+      <li
+        :class="[route.path == '/work' ? activeLinkClasses : '', linkClasses]"
+      >
         <nuxt-link to="/work">Work</nuxt-link>
       </li>
-      <li :class="route.path == '/blog' ? activeLinkClasses : linkClasses ">
+      <li
+        :class="[route.path == '/blog' ? activeLinkClasses : '', linkClasses]"
+      >
         <nuxt-link to="/blog">Blog</nuxt-link>
       </li>
       <li :class="linkClasses">
@@ -19,7 +23,5 @@ const route = useRoute();
 
 const linkClasses =
   "text-lg after:content-[' '] after:w-0 after:bg-blue-700 after:h-[2px] after:block hover:after:w-full transition-all after:transition-all after:duration-300 after:ease-in-out hover:opacity-50 duration-300 ease-in-out";
-
-const activeLinkClasses =
-  "text-lg after:content-[' '] after:w-0 after:bg-blue-700 after:h-[2px] after:block hover:after:w-full transition-all after:transition-all after:duration-300 after:ease-in-out hover:opacity-50 duration-300 ease-in-out text-blue-700";
+const activeLinkClasses = "text-blue-700";
 </script>
